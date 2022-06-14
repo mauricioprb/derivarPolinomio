53ef320d710f6dfe6c6c7c01bb050b5b849ac2f3
mauricioprb <mauriciopereirabraga@gmail.com>
2022-06-14 13:15:19 -0300

    derivada

---
+++ src/App.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        public void deriva() {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Digite o grau: ");
+            grau = Integer.parseInt(scanner.nextLine());
+
+            int i = 0;
+
+            while(i > grau) {
+
+            }
+        }
+    }
+}
